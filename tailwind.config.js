258399d1812f79cfc4e32b1e5548b1101edb57bb
codes-by-chetan <chetanmohite2128@gmail.com>
2025-05-08 08:30:42 +0530

    tailwind config converted to es6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+ export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -97,5 +97,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [import("tailwindcss-animate")],
 };
