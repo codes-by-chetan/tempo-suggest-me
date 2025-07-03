7c46a3506e5e0cb1535cb1b5892ee3637069ac86
Chetan Mohite <158462483+codes-by-chetan@users.noreply.github.com>
2025-07-03 19:17:49 +0530

    Updated env.config.ts

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,6 +1,6 @@
 const dev = {
-  API_URL: "http://192.168.0.39:3200/api/",
-  SOCKET_URL: "http://192.168.0.39:3200",
+  API_URL: "https://suggest-me.onrender.com/api/",
+  SOCKET_URL: "https://suggest-me.onrender.com",
 };
 
 const prod = {
